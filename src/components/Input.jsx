5d792860b0ba6222d8eb7ddb76d80447286bdc89
iftekharalammithu45 <iftekharalammithu45@gmail.com>
2024-06-27 14:52:46 +0000

    update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import attach from "../img/attach.png";
+import img from "../img/img.png";
 
 const Input = () => {
-  return <div className="input">Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type a message...." />
+      <div className="send">
+        <img src={img} alt="" />
+        <input style={{ display: "none" }} type="file" id="file" />
+        <label htmlFor="file">
+          <img src={attach} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="messageinfo">
+        <img src="src/img/download (3).jpg" alt="user-profile" />
+        <span>User Name</span>
+      </div>
+      <div className="messagecontent">
+        <p>hello</p>
+        <img src="src/img/download (3).jpg" alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
